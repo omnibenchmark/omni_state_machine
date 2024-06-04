20576bf73539722401417862e9cd5a13ccf0631e
Daniel Incicau <daniel_incicau@yahoo.com>
2024-06-04 22:23:21 +0200

    Write documentation for the workflow engine interface.

--- src/workflow/workflow.py
+++ src/workflow/workflow.py
@@ -9,14 +9,38 @@ class WorkflowEngine:
         self.benchmark = benchmark
 
     def run_workflow(self):
+        """
+        Serializes & runs benchmark workflow.
+
+        Returns:
+        - Status code of the workflow run.
+        """
         raise NotImplementedError("Method not implemented yet")
 
     def serialize_workflow(self, output_path=os.getcwd()):
+        """
+        Serializes a workflow file for the benchmark.
+
+        Returns:
+        - Workflow file path.
+        """
         raise NotImplementedError("Method not implemented yet")
 
     def run_node_workflow(self, node):
+        """
+        Serializes & runs benchmark node's workflow.
+
+        Returns:
+        - Status code of the workflow run.
+        """
         raise NotImplementedError("Method not implemented yet")
 
     def serialize_node_workflow(self, node, output_path=os.getcwd()):
+        """
+        Serializes a workflow file for a benchmark node.
+
+        Returns:
+        - Workflow file path.
+        """
         raise NotImplementedError("Method not implemented yet")
 
